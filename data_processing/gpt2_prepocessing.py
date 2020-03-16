0b9fcaedea8a2ad91a47c399ea3e285dcaae772f
vanh17 <vanh17@hanover.edu>
2020-03-15 20:51:59 -0700

    added gpt2 processing

---
+++ data_processing/gpt2_prepocessing.py
@@ -0,0 +1,15 @@
+import sys
+import io
+
+def main():
+	if len(sys.argv) < 3:
+		print("[usage]: python gpt2_preprocessing.py normal.aligned single.aligned processed.txt")
+		return None
+	with io.open(sys.argv[1], "r") as normal, io.open(sys.argv[2], "r") as simple, io.open(sys.argv[3], "w") as processed:
+		normal = normal.read().splitlines()
+		simple = simple.read().splitlines()
+		for i in range(len(normal)):
+			processed.write("|<startoftext>|" + normal[i].split("\t")[2] + " |<sep>| " + simple[i].split("\t")[2] + " |<endoftext>|\n")
+
+if __name__ == '__main__':
+	main()
