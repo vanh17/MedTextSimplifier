3dd6f6328b799e32004281f3e45101c8afe4d981
vanh17 <vanh17@hanover.edu>
2020-03-16 16:00:12 -0700

    Removed reset grpah

--- gpt2_finetuning.py
+++ gpt2_finetuning.py
@@ -1,6 +1,5 @@
 import gpt_2_simple as gpt2
 import os
-import tensorflow as tf
 import sys
 
 def gpt2_finetuning(model_name, data_file, step):
@@ -10,7 +9,6 @@ def gpt2_finetuning(model_name, data_file, step):
 
     sess = gpt2.start_tf_sess()
     file_name = data_file
-    tf.reset_default_graph()
     '''
    	For finetuning use this command. Following are other parameters of the finetuning function:
         sess,
