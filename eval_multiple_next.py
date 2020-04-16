0b1ed5c9d739d5377d00cec966b91565cee42799
vanh17 <vanh17@hanover.edu>
2020-04-16 13:32:05 -0700

    Modify multiple 5 preds

--- eval_multiple_next.py
+++ eval_multiple_next.py
@@ -14,24 +14,31 @@ def eval(preds, labels, threshold):
 	labels = open(labels, "r").readlines()
 	for i in range(len(labels)):
 		total += 1
+		# initialize satisfactory
+		satisfactory = False
 		# spacy similarity
-		pred = nlp(preds[i].strip("\n").lower())
-		print(pred)
-		label = nlp(labels[i].strip("\n").lower())
-		print(label)
-		if 	pred.similarity(label) >= threshold:
-		# difflib similarity
-		# label = labels[i].strip("\n").lower()
-		# pred = preds[i].strip("\n").lower()
-		# if difflib.SequenceMatcher(None, pred, label).ratio() >= threshold:
-			corrected += 1
+		pred_list = preds[i].strip("\n").lower().split(" ")
+		# this label might be have multiple words at a time
+		label_list = labels[i].strip("\n").lower().split(" ")
+		count = 0
+		for t in range(len(label_list)):
+			pred = nlp(pred_list[t])
+			label = nlp(label_list[t]) 
+			if 	pred.similarity(label) >= threshold:
+			# difflib similarity
+			# label = labels[i].strip("\n").lower()
+			# pred = preds[i].strip("\n").lower()
+			# if difflib.SequenceMatcher(None, pred, label).ratio() >= threshold:
+				count += 1
+		if count == len(label_list):
+			corrected += (count / len(label_list))
 	print("Accuracy: ", corrected / total)
 
 def main():
 	if len(sys.argv) != 4:
 		print("[usage]: python3 eval.py preds_file_name labels_file_name threshold")
 	else:
-		# python3 eval.py data_processing/preds/roberta/rob_context_preds.txt data_processing/data/dev_labels.txt .99
+		# python3 eval_multiple_next.py data_processing/preds/roberta/rob_context_preds_2.txt data_processing/data/next2/dev_labels_2.txt .99
 		eval(sys.argv[1], sys.argv[2], float(sys.argv[3]))
 
 if __name__ == '__main__':
